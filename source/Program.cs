65c00d663bc3a78f7e10ac929f5a751ae5192de1
Elimination <lorder222@rambler.ru>
2016-04-11 13:11:50 +0700

    Rename and add missing files

--- source/Program.cs
+++ source/Program.cs
@@ -8,9 +8,7 @@ namespace Rsa_algo
 {
     class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        // The main entry point for the application.
         [STAThread]
         static void Main()
         {
